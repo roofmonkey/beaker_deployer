b1a0428a9e179a43b165a465abd13dfaf5484e3c
jayunit100 <jayunit100@gmail.com>
2013-06-10 09:50:25 -0400

    bootstrapper for beaker initialization

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:roofmonkey/SystemTests.git
+git clone git@github.com:roofmonkey/self_contained_install.git
+git clone git@github.com:roofmonkey/beaker_deployer.git
+cd beaker_deployer/
+#Edit this with the correct server list.
+cat >servers << EOF
+mrg19
+mrg41
+mrg42
+mrg44
+EOF
