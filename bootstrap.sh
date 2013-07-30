cfeb4a983a698cfe33cedb726015c88fcabda0c4
root <root@mrg40.lab.bos.redhat.com>
2013-07-30 00:28:09 -0400

    more tiny tweaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,25 @@
+cd /root/
+
 git clone git@github.com:roofmonkey/SystemTests.git
 git clone git@github.com:roofmonkey/self_contained_install.git
 git clone git@github.com:roofmonkey/beaker_deployer.git
-cd beaker_deployer/
 
 echo "About to setup servers file : are the servers in this script up to date ? If not cancel and edit this script".
 
 #Edit this with the correct server list.
 cat >servers << EOF
-mrg19
-mrg22
-mrg41
+mrg40
+mrg42
+mrg43
 mrg44
 EOF
 
 #Copy over updated slaves into "slaves" file. 
-cp servers ./self_contained_install/deploy/slaves
+cp ./servers ./self_contained_install/deploy/slaves
 
 echo "Comment out the IFCONFIG script in BEAKER and click enter to proceed...also, set passwords on all nodes so that you can enter them"
 echo "Press enter when ready to proceed."
 read x
 
 #Setup passwordless ssh 
-./ifconfig.ssh
+/root/beaker_deployer/ifconfig.sh servers
