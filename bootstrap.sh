ae3abb5788ac0998d389efc04b772c9e0f55047c
root <root@mrg40.lab.bos.redhat.com>
2013-08-06 11:25:51 -0400

    autovalidate servers on creating cluster, that they are both named (dns) and reachable (<100% packet loss)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd /root/
 
 git clone --recursive git@github.com:roofmonkey/SystemTests.git
@@ -10,12 +11,28 @@ echo "About to setup servers file : are the servers in this script up to date ? 
 cat >servers << EOF
 mrg40
 mrg42
-mrg43
+mrg19
 mrg44
 EOF
 
+###### Validate that servers are reachable before proceeding ##### 
+slaves=( `cat "servers" `)
+for node in "${slaves[@]}"
+  do
+    echo "pinging $node"
+    unreachable=`ping -c4 $node 2>&1 | awk '/100% packet loss|unk/'`
+    if [ ! "$unreachable" == "" ]; then
+        echo "$node unreachable. 
+              <<< $unreachable >>> 
+        Update the servers in the above declaration and re run this script"
+        exit
+    fi
+    echo "   success ! $node : $unreachable"
+done
+
+
 #Copy over updated slaves into "slaves" file. 
-cp ./servers ./self_contained_install/deploy/slaves
+cp /root/servers /root/self_contained_install/deploy/slaves
 
 echo "Comment out the IFCONFIG script in BEAKER and click enter to proceed...also, set passwords on all nodes so that you can enter them"
 echo "Press enter when ready to proceed."
